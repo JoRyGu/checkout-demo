d8ea568a2c3e0e6b5e58fe3642770667eaae25f4
Josh <josh.gude@mentistic.com>
2023-03-25 17:43:09 -0400

    Added aws secrets manager sdk to redirect handler

--- apps/cloud/lambdas/checkoutRedirect/handler.ts
+++ apps/cloud/lambdas/checkoutRedirect/handler.ts
@@ -1,10 +1,21 @@
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { z } from 'zod';
 
 const redirectRequestSchema = z.object({});
 
+const getStripeSecret = async () => {
+  const client = new SecretsManagerClient({ region: 'us-east-1' });
+  const command = new GetSecretValueCommand({ SecretId: 'StripeSecret' });
+  return client.send(command);
+};
+
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  console.log(event);
+  const stripeKey = await getStripeSecret();
+  console.log({ stripeKey });
 
   return {
     statusCode: 200,
